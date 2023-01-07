6f09d4af8b34b64706988a1a170021bea73dcb96
Lais Lobato <laisrlobato@gmail.com>
2023-01-07 17:38:21 -0300

    Setup next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,25 @@
+import { Inter } from "@next/font/google"
+
 import type { AppProps } from "next/app"
 
+const inter = Inter({
+  subsets: ["latin"],
+  fallback: ["Helvetica", "sans-serif"],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+
+        button {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
