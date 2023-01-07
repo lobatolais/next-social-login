aea2cfc14173135b3b3d50fd66af4e6398e4d532
Lais Lobato <laisrlobato@gmail.com>
2023-01-07 18:17:11 -0300

    Define global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { Inter } from "@next/font/google"
 
 import type { AppProps } from "next/app"
 
+import "../styles/globals.scss"
+
 const inter = Inter({
   subsets: ["latin"],
   fallback: ["Helvetica", "sans-serif"],
